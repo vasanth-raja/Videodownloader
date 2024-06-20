1297124cbca6629431c2b3623810316878fc18d9
vasanth-raja <vasanthrajark@gmail.com>
2024-06-20 12:18:58 +0530

    ui changes

--- VideoDownloader/src/component/LinkBox.jsx
+++ VideoDownloader/src/component/LinkBox.jsx
@@ -3,8 +3,10 @@ import '../Styles/LinkBox.css'
 const LinkBox = () => {
     const [link,setLink]=useState("");
     const [url,setUrl]=useState("");
+    const [isLoading,setIsLoading]=useState(false)
     const submitForm=async(e)=>{
         e.preventDefault();
+        setIsLoading(true)
         const res= await fetch('http://localhost:5000/download',{
             method:'POST',
             headers:{
@@ -13,26 +15,31 @@ const LinkBox = () => {
             body:JSON.stringify({
                 'url':link
             })
+        }).catch(err=>{
+            setIsLoading(false)
+            console.log(err)
         })
+        setIsLoading(false)
         const data=await res.json()
         console.log(data)
         setUrl(data.url)
     }
   return (
    
-    <div>
-        <form onSubmit={submitForm}>
+    <div className='box' >
+        <form onSubmit={submitForm} >
             <div>
-                <label htmlFor='link'><h2>Enter Your Link here</h2></label>
+                <label htmlFor='link' className='Bold'><h2>Enter Your Link here</h2></label>
                 <input onChange={(e)=>{setLink(e.target.value)}} type='text' placeholder='Link to donwload video' name='link' id='link' className='link' value={link}/>
             </div>
             <div>
                 <button className='submit' type='submit'><h2>Submit</h2></button>
             </div>
         </form>
-        {url && (<div> <video width="320" height="240" controls>
-            <source src={url} type="video/mp4"></source>  
-        </video> <a  href={url} download={"LinkedInd"}>download</a></div>)}
+        <div className='loading'>
+            {isLoading ? <p>Loading...</p> : null}
+        </div>
+        {url && (<div className='down'><a  href={url} download={"LinkedInd"}>download</a></div>)}
        
     </div>
   )
